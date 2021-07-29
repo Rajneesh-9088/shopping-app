3d5e7a9a524b5e2308c1171e3654864443b871e3
proraj9088 <rp8735685@gmail.com>
2021-07-29 18:40:36 +0530

    show buy product page

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -15,6 +15,20 @@ router.get('/products', async (req,res)=>{
    
 })
 
+router.post('/products', async(req,res)=>{
+
+ const product =  await Product.create(req.body);
+
+ res.status(200).json(product);
+
+
+})
+
+router.get('/products/:id', async(req,res)=>{
+    const product = await Product.findById(req.params.id);
+    res.json(product);
+})
+
 
 
 
